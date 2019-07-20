c11446e1f1d0c4a9e8e6ab8c7efe1b4b897ae328
xiaoweiol <463644535@qq.com>
2019-07-20 22:27:25 +0800

    抖音的桌面小程序

---
+++ 你喜欢我吗？/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace 你喜欢我吗_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("华文楷体", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(105, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hey，小姐姐，你喜欢我吗？";
+            this.label1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "喜欢";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "不喜欢";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(111, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 297);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 376);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 你喜欢我吗？/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 你喜欢我吗_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.BringToFront();
+            resetLocation();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("太好了！\n小姐姐，我也喜欢你！！！\n做我女朋友吧！\n爱你，么么哒！");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            int X = button2.Location.X+75;
+            int Y = button2.Location.Y+23;
+            if(X>this.Size.Width - 75){
+                X = 0;
+            }
+            if(Y>this.Size.Height - 50){
+                Y = 0;
+            }
+            Point point = new Point(X,Y);
+            button2.Location = point;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("拒绝我？！不存在的！嘻嘻");
+        }
+
+        private void fun1()
+        {
+            DialogResult result = MessageBox.Show("我喜欢你！", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("太好了！\n小姐姐，我也喜欢你！！！\n做我女朋友吧！\n爱你，么么哒！");
+            }
+            else
+            {
+                fun2();
+            }
+        }
+
+        private void fun2()
+        {
+            DialogResult result = MessageBox.Show("房产证写你名字", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun3();
+            }
+        }
+
+        private void fun3()
+        {
+            DialogResult result = MessageBox.Show("保大", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun4();
+            }
+        }
+
+        private void fun4()
+        {
+            DialogResult result = MessageBox.Show("我妈会游泳", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun5();
+            }
+        }
+
+        private void fun5()
+        {
+            DialogResult result = MessageBox.Show("我有钱", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun6();
+            }
+        }
+
+        private void fun6()
+        {
+            DialogResult result = MessageBox.Show("我有房", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun7();
+            }
+        }
+
+        private void fun7()
+        {
+            DialogResult result = MessageBox.Show("我有车", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun8();
+            }
+        }
+
+        private void fun8()
+        {
+            DialogResult result = MessageBox.Show("做我女朋友好不好", "选择", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                like();
+            }
+            else
+            {
+                fun1();
+            }
+        }
+
+        
+
+        private void like()
+        {
+            MessageBox.Show("太好了！\n小姐姐，我也喜欢你！！！\n做我女朋友吧！\n爱你，么么哒！");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            fun1();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            resetLocation();
+            
+        }
+
+        private void resetLocation()
+        {
+            int button1X = 0 + 100;
+            int button2X = this.Size.Width - 200;
+            int button1Y = this.Size.Height - 100;
+            int button2Y = this.Size.Height - 100;
+            button1.Location = new Point(button1X, button1Y);
+            button2.Location = new Point(button2X, button2Y);
+        }
+    }
+}
